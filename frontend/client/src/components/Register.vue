107d85ac198e9379f76ace10d21cb56e448d322e
Ronit Wilson <ronit.study@gmail.com>
2024-01-28 20:54:33 +0530

    start with integrating vuetify

--- frontend/client/src/components/Register.vue
+++ frontend/client/src/components/Register.vue
@@ -1,23 +1,31 @@
 <template>
-  <div>
-  <h1> Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense calss="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email"/>
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password"/>
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn @click="register"> Register </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 
-  <input
-  type="email"
-  name="email"
-  v-model="email"
-  placeholder="email"/>
-  <br>
-  <input
-  type="password"
-  name="password"
-  v-model="password"
-  placeholder="password"/>
-  <br>
-  <div class="error" v-html="error"></div>
-  <br>
-  <button @click="register"> Register </button>
-</div>
 </template>
 
 <script>
