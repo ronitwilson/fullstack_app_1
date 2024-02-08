f6f6b80e10f32e7dfe0d9384635cf7abba02c775
Ronit Wilson <ronit.study@gmail.com>
2024-02-08 09:23:49 +0530

    add header componenet

--- frontend/client/src/App.vue
+++ frontend/client/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <page-header/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import PageHeader from '@/components/Header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    PageHeader
+  }
 }
 </script>
 
---
+++ frontend/client/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+        <v-toolbar title  dense >
+          <v-toolbar-title class="mr-4">Tabtracker</v-toolbar-title>
+        </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+  <style scoped>
+
+  </style>
